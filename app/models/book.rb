a898bab778b4fb307c85fa59f6452840d3793554
Karen S Kim <karenskim19@gmail.com>
2015-01-22 17:35:58 -0800

    add title tests for uniqueness and stringiness

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :authorname, presence: true
   validates :genre, presence: true
   validates :summary, presence: true
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -9,12 +9,22 @@ RSpec.describe Book, :type => :model do
     expect(FactoryGirl.build(:book, title: nil)).to be_invalid
   end
 
+  it "cannot be created with a title that already exists" do
+    book = (FactoryGirl.create(:book, title: "MyString"))
+    book2 = (FactoryGirl.build(:book, title: "MyString"))
+    expect(book2).to be_invalid
+  end
+
   it "it returns a book title as a string" do
     expect(FactoryGirl.build(:book).title).to be_an_instance_of(String)
   end
 
   it "is invalid without an author name" do
     expect(FactoryGirl.build(:book, authorname: nil)).to be_invalid
+  end
+
+  it "has an author name longer than three characters" do
+    expect(FactoryGirl.build(:book, authorname: "hi")).to be_invalid
   end
 
   it "is invalid without a genre" do
