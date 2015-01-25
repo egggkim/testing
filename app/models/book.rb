d9d9e529e2872c48440f31475df5753d05b4d034
Karen S Kim <karenskim19@gmail.com>
2015-01-25 08:24:11 -0800

    fix model spec to validate for minimum length of author name

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: true
   validates :authorname, presence: true
+  validates_length_of :authorname, :minimum => 3
   validates :genre, presence: true
   validates :summary, presence: true
 
